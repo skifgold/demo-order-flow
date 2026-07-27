ae2992604f76983533846194cb74d557c73c5d68
Bohdan Paperovskyi <b.paperovskyi@gmail.com>
2026-07-27 19:29:33 +0300

    feat: add new libs
    - zod
    - primevue v4 as main ui lib. PrimeVue v5 is skipped because of new
    license policy
    - msw for mocking response
    - tanstack query

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,7 +17,7 @@ export default defineConfigWithVueTs(
     files: ['**/*.{vue,ts,mts,tsx}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'public/mockServiceWorker.js']),
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
