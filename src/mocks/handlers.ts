dd382d81de92deb2164fdb94d22513143beb857e
Bohdan Paperovskyi <b.paperovskyi@gmail.com>
2026-07-27 21:13:12 +0300

    feat: add delay to the mock handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,11 @@
-import { HttpResponse, http } from 'msw'
+import { HttpResponse, delay, http } from 'msw'
 
 import { catalogueProducts } from './catalogue.data'
 
 export const handlers = [
-  http.get('*/products', () => {
+  http.get('*/products', async () => {
+    await delay(250)
+
     return HttpResponse.json(catalogueProducts)
   }),
 ]
